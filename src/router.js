8ce42fc4d829aa19e52b91c9b78c65f2e25bbb3d
Dylan <46438287+dylan237@users.noreply.github.com>
2019-07-12 10:39:09 +0800

    Update router.js

--- src/router.js
+++ src/router.js
@@ -4,7 +4,7 @@ import Home from './views/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -21,3 +21,32 @@ export default new Router({
     }
   ]
 })
+
+// 導航守衛(當跳頁時觸發)
+// to:要去的路由 / form:從哪個路由 / next:通過驗證 
+// router.beforeEach((to, from, next) => {
+//   // 如果要前往的頁面需要驗證(有在router.js的頁面元件設定to.meta.requiresAuth)
+//   if (to.meta.requiresAuth) {
+//     console.log('這個跳頁需要驗證')
+//     //觸發驗證用的API
+//     const api = `${process.env.VUE_APP_CUSTOMPATH}/api/user/check`;
+//     // this.$http.post(api).then((response) => {
+//     // 上一行是錯誤的示範，`this.$http`是在VUE元件內才可以呼叫的，在全域無法使用
+//     axios.post(api).then((response) => {
+//       console.log(`是否有登入：${response.data.success}`); // 伺服器回傳是否有持續登入
+//       if(response.data.success) {
+//         next();
+//       } else {
+//         next({
+//           path: '/login' // 如果判斷沒有持續登入，用next帶入物件，並導向該物件的path屬性對應的值
+//         })
+//       }
+//     })
+//   } 
+//   // 不需要驗證則放行
+//   else {
+//     next();
+//   }
+// });
+
+export default router;
